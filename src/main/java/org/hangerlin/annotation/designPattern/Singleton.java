f861dcf0bda4546c62e092398bb356ade329eb96
Tinghan Lin <20222131084@m.scnu.edu.cn>
2024-02-07 09:44:52 +0800

    first commit

---
+++ src/main/java/org/hangerlin/annotation/designPattern/Singleton.java
@@ -0,0 +1,8 @@
+package org.hangerlin.annotation.designPattern;/** 
+* @description TODO 
+* @author lth 
+* @date 2024/2/7 09:39 
+* @version 1.0 
+*/
+public class Singleton {
+}
---
+++ src/main/java/org/hangerlin/designPattern/Singleton.java
@@ -0,0 +1,10 @@
+package org.hangerlin.designPattern;
+
+/**
+ * @author lth
+ * @version 1.0
+ * @description TODO
+ * @date 2024/2/7 09:39
+ */
+public class Singleton {
+}
